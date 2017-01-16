80e796be7373d7e01d7988d8efcc08e7c5c6934b
Ashley Heilman <ajheilman@lcmail.lcsc.edu>
2017-01-15 20:58:59 -0800

    Update bottles.rb

--- bottles.rb
+++ bottles.rb
@@ -1,6 +1,7 @@
 bottles = 99
 while bottles > 0
 	if bottles == 1
+	#Created this if statement as it should be '1 bottle', not '1 bottles' when it goes to 1. 
 		puts "#{bottles} bottle of beer on the wall\n"
 		puts "#{bottles} bottle of beer "
 		puts "Take one down, pass it around"
@@ -12,6 +13,7 @@ while bottles > 0
 		puts "Take one down, pass it around"
 		bottles = bottles - 1
 		if bottles == 1
+		#It should be '1 bottle', not '1 bottles' when it goes to 1.
 			puts "#{bottles} bottle of beer on the wall"
 		else
 			puts "#{bottles} bottles of beer on the wall"
